c2a6bcccc3956d5622f93babe722029a3efb1219
unknown <rathorgaurav92@gmail.com>
2018-01-28 12:59:46 +0530

    firstcommit

---
+++ com.seleniumAutomation.selenumProject/src/test/java/com/seleniumAutomation/selenumProject/app/Module1.java
@@ -0,0 +1,12 @@
+package com.seleniumAutomation.selenumProject.app;
+
+import org.testng.annotations.Test;
+
+public class Module1 {
+          @Test
+	public static void mainTest() {
+		System.out.println("maven start");
+
+	}
+
+}
